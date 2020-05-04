8583ba4d41e26a068a19bc1e5b794fcc488ff61d
Ali-Sharifi-Neyestani <sh4rifi@gmail.com>
2020-05-04 20:53:27 +0430

    add models

---
+++ app/Models/DailyMessage.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class DailyMessage extends Model
+{
+    protected $table = 'daily_messages';
+}
---
+++ app/Models/LifeLog.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class LifeLog extends Model
+{
+    protected $table = 'life_logs';
+}
